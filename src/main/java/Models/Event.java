fc72eca9eb395f46f9c7cd9d241e1baf1b90e04e
nr <nicholas.raethke@gmail.com>
2017-08-04 09:13:21 -0700

    createe event planner project and set up Event class.

---
+++ src/main/java/Models/Event.java
@@ -0,0 +1,20 @@
+package Models;
+
+public class Event {
+    public int Guests;
+    public String [] DinnerMenu;
+    public String [] Libations;
+    public String Entertainment;
+    public Event(int Guests,, String DinnerMenu[], String [] Libations, String Entertainment){
+        this.Guests = Guests;
+        this.DinnerMenu[] = DinnerMenu[];
+        this.
+    }
+
+
+
+    public static void Estimate(){
+        //create price based on event input
+
+    }
+}
---
+++ src/test/java/Models/EventTest.java
@@ -0,0 +1,16 @@
+package Models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EventTest {
+    @Test
+    public void newGuest_returnGuestNum_1() {
+        Guests testGuests =  new Guests
+        assert
+
+
+    }
+
+}
