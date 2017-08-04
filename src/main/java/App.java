ff3d027eca8de5f00de234937de6fa2ac265d101
nr <nicholas.raethke@gmail.com>
2017-08-04 15:48:50 -0700

    allow user to go through prompts and return price for single string in each variable

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,30 +3,71 @@ import Models.Event;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
-//public class App {
-//
-//    public static void main(String[] args) {
-//
-//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-//
-//
-//        boolean programRunning = true;
-//
-//        Event newEvent = new Event();
-//
-//        while (programRunning)
-//
+public class App {
+
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+
+        boolean programRunning = true;
+        int userGuests = 0;
+        List<String> userDinnerMenu = new ArrayList<>();
+        List<String> userLibations = new ArrayList<>();
+        List<String> userEntertainment = new ArrayList<>();
+        String userInput;
+
+        //Event newEvent = new Event(int Guests, List<> food, bev, party);
+
+
+
 //        try {
-//            System.out.println("Welcome to Entertainment Excellence Event Estimator" +
-//                    "Please tell us how many people will be attending your event");
-//            userInput = bufferedReader.readLine();
-//
-//            Event userEvent = new Event();
+            while (programRunning) {
+                System.out.println("Welcome to Entertainment Excellence Event Estimator" + " You may type '0' to exit. " +
+                        "Please tell us how many people will be attending your event");
+                userInput = bufferedReader.readLine();
+                userGuests = Integer.parseInt(userInput);
+                if (userInput.equals("q")) {
+                    programRunning = false;
+                } else {
+                    //
+                    System.out.println("Which courses will your guests be having, 'mezza', 'shawarma', 'dessert'");
+                    userInput = bufferedReader.readLine();
+                }
+                    if (userInput.equals("q")) {
+                        programRunning = false;
+                    } else {
+                        userDinnerMenu.add(userInput);
+                        System.out.println("Which beverages will your guests be having, 'beer and wine', 'spirits', 'nonalcoholic'");
+                        userInput = bufferedReader.readLine();
+                    }
+                        if (userInput.equals("q")) {
+                            programRunning = false;
+                        } else {
+                            userLibations.add(userInput);
+                            System.out.println("Which entertainment will your guests be enjoying, 'DJ', 'String Quartet', 'Band'");
+                            userInput = bufferedReader.readLine();
+                        }
+                            if (userInput.equals("q")) {
+                                programRunning = false;
+                            } else {
+                                userEntertainment.add(userInput);
+                            }
+
+
+                Event userEvent = new Event(userGuests, userDinnerMenu, userLibations, userEntertainment);
+
+
+                System.out.println("Your estimated cost is $" + userEvent.giveEstimate());
+
+            }
 //        }
 //        catch (IOException e) {
 //            e.printStackTrace();
 //        }
-//
-//    }
-//}
+
+    }
+}
