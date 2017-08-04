4c57241faad056a2d29d08c4a69f13a0e9987369
nr <nicholas.raethke@gmail.com>
2017-08-04 10:08:08 -0700

    pass or fail first test based on inputted number of Guests.

--- src/main/java/Models/Event.java
+++ src/main/java/Models/Event.java
@@ -1,19 +1,39 @@
 package Models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Event {
-    public int Guests;
-    public String [] DinnerMenu;
-    public String [] Libations;
-    public String Entertainment;
-    public Event(int Guests,, String DinnerMenu[], String [] Libations, String Entertainment){
+    private int Guests = 0;
+    private List<String> DinnerMenu = new ArrayList<>();
+    private List<String> Libations = new ArrayList<>();
+    private List<String> Entertainment = new ArrayList<>();
+
+
+    public Event(int Guests, List<String> DinnerMenu, List<String> Libations, List<String> Entertainment){
         this.Guests = Guests;
-        this.DinnerMenu[] = DinnerMenu[];
-        this.
+        this.DinnerMenu = DinnerMenu;
+        this.Libations = Libations;
+        this.Entertainment = Entertainment;
     }
 
+    public int getGuests() {
+        return Guests;
+    }
 
+    public List<String> getDinnerMenu() {
+        return DinnerMenu;
+    }
 
-    public static void Estimate(){
+    public List<String> getLibations() {
+        return Libations;
+    }
+
+    public List<String> getEntertainment() {
+        return Entertainment;
+    }
+
+    public static void giveEstimate(){
         //create price based on event input
 
     }
--- src/test/java/Models/EventTest.java
+++ src/test/java/Models/EventTest.java
@@ -1,16 +1,20 @@
 package Models;
 
 import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class EventTest {
     @Test
-    public void newGuest_returnGuestNum_1() {
-        Guests testGuests =  new Guests
-        assert
-
-
+    public void newGuest_returnGuestNum_1() throws Exception {
+        int testGuests = 1;
+        List<String> testDinnerMenu = new ArrayList<>();
+        List<String> testLibations = new ArrayList<>();
+        List<String> testEntertainment = new ArrayList<>();
+        Event testEvent = new Event(testGuests, testDinnerMenu, testLibations, testEntertainment);
+        assertEquals(1, testEvent.getGuests());
     }
 
 }
